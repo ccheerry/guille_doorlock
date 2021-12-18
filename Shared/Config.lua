8d116024e5bd663f0135040dde3cf329fe79e2ff
CherryozZ <81585398+CherryozZ@users.noreply.github.com>
2021-12-19 00:02:31 +0100

    feat(slider) updated slider doors

--- Shared/Config.lua
+++ Shared/Config.lua
@@ -2,7 +2,7 @@ Config = {} or Config
 
 Config['enableNuiIndicator'] = true
 
-Config['scriptVersion'] = 1.1
+Config['scriptVersion'] = 1.2
 
 Config["strings"] = {
     ['open'] = "Press ~b~E~w~ to ~g~open~w~ the door",
